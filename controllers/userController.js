5f0d949cc05a0f159e41d1952841fd868cc09830
Kayne <kayne6@mail.com>
2022-02-23 08:55:25 -0800

    finished slide 48

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,42 @@
 const {User} = require('../models');
+const md5 = require('md5');
+const passport = require('passport');
 
 module.exports.renderRegistration = function (req,res){
     res.render('users/register');
 };
 
 module.exports.register = async function(req,res) {
-    await User.create( {
-        email: req.body.email,
-        password: req.body.password,
-        first_name: req.body.first_name,
-        last_name: req.body.last_name
+    const existingUser = await User.findOne({
+        where: {
+            email:req.body.email
+        }
     });
-    res.redirect('/');
+    if (existingUser){
+        res.render('users/register', {
+            error: 'User already exists'
+        })
+    } else {
+    await User.create( {
+            email: req.body.email,
+            password: md5(req.body.password),
+            first_name: req.body.first_name,
+            last_name: req.body.last_name
+        });
+        res.redirect('/');
+    }
 }
+
+module.exports.renderLogin = function (req,res) {
+    let error = null
+    if (req.session.messages && req.session.messages.length>0){
+        error = req.session.messages[0];
+    }
+    res.render('users/login', {error});
+}
+
+module.exports.authenticate = passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureMessage: true
+})
